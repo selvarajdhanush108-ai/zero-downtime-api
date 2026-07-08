9f280b3560aa035a289224af5519ee4b36cbf844
Dhanush S <selvarajdhanush108@gmail.com>
2026-07-08 16:58:23 +0530

    Add DockerHub CI pipeline

--- src/main/java/com/zetheta/zero_downtime_api/controller/HelloController.java
+++ src/main/java/com/zetheta/zero_downtime_api/controller/HelloController.java
@@ -8,11 +8,31 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to Zero Downtime CI/CD Pipeline!";
+        return "Welcome to Zero Downtime CI/CD Pipeline!, v3";
     }
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, DevOps!";
+        return "Hello, DevOps!, v3";
     }
+    @GetMapping("/health")
+    public String health() {
+        return "Application is healthy!, v3";   
+    }
+    @GetMapping("/version")
+    public String version() {
+        return "Application version: v3";   
+    }
+    @GetMapping("/info")
+    public String info() {
+        return "This is a sample application to demonstrate zero downtime deployment using Kubernetes and Spring Boot!, v3";   
+    }
+    @GetMapping("/status")
+    public String status() {
+        return "Application is running smoothly!, v3";  
+    }
+    @GetMapping("/selector")
+    public String selector() {
+        return "Green";   
+    }   
 }
